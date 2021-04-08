1d02240cd839e8d6aa6b8e85ff0eb5dbe93fe563
Kajutin <65399531+Kajutin@users.noreply.github.com>
2021-04-08 17:20:59 +0300

    Add files via upload
    Šiandienos darbas:
    Perkėliau darbą į .NET CORE aplinką, nes joje daugiau funkcijų dirbant su RSA šifravimu.
    Nauji dalykai:
    > Naujas šifravimo kodas;
    > Naujas dešifravimo kodas;
    > Veikia šifravimas iš failo;
    > Išsaugomas viešasis raktas į atskirą failą.
    
    Nepadaryta:
    Negalima pakeisti parametrų pačiam (p ir q). Tačiau viskas apart to veikia.

---
+++ Sifravimas V3/Sifravimas/Sifravimas V3.2/Sifravimas V3.2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Sifravimas_V3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LibraryRawTextBox = new System.Windows.Forms.TextBox();
+            this.LibraryEncodedTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LibraryDecryptButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LibraryRawTextBox
+            // 
+            this.LibraryRawTextBox.Location = new System.Drawing.Point(12, 12);
+            this.LibraryRawTextBox.Multiline = true;
+            this.LibraryRawTextBox.Name = "LibraryRawTextBox";
+            this.LibraryRawTextBox.Size = new System.Drawing.Size(301, 138);
+            this.LibraryRawTextBox.TabIndex = 0;
+            this.LibraryRawTextBox.Text = "TEXT TO ENCRYPT GOES HERE";
+            // 
+            // LibraryEncodedTextBox
+            // 
+            this.LibraryEncodedTextBox.Location = new System.Drawing.Point(335, 12);
+            this.LibraryEncodedTextBox.Multiline = true;
+            this.LibraryEncodedTextBox.Name = "LibraryEncodedTextBox";
+            this.LibraryEncodedTextBox.Size = new System.Drawing.Size(301, 138);
+            this.LibraryEncodedTextBox.TabIndex = 1;
+            this.LibraryEncodedTextBox.Text = "ENCRYPTED TEXT GOES HERE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LibraryDecryptButton
+            // 
+            this.LibraryDecryptButton.Location = new System.Drawing.Point(335, 156);
+            this.LibraryDecryptButton.Name = "LibraryDecryptButton";
+            this.LibraryDecryptButton.Size = new System.Drawing.Size(95, 23);
+            this.LibraryDecryptButton.TabIndex = 3;
+            this.LibraryDecryptButton.Text = "Decrypt";
+            this.LibraryDecryptButton.UseVisualStyleBackColor = true;
+            this.LibraryDecryptButton.Click += new System.EventHandler(this.LibraryDecryptButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Decrypt from file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 203);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.LibraryDecryptButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LibraryEncodedTextBox);
+            this.Controls.Add(this.LibraryRawTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LibraryRawTextBox;
+        private System.Windows.Forms.TextBox LibraryEncodedTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button FileDecryptButton;
+        private System.Windows.Forms.Button LibraryDecryptButton;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Sifravimas V3/Sifravimas/Sifravimas V3.2/Sifravimas V3.2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime;
+
+namespace Sifravimas_V3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LibraryRawTextBox.MaxLength = 58;
+            //
+            // RSA nepalaiko ilgo teksto, tada jau reikia kombinuoti su DES ar AES sifrais, jei nori ilgesnio
+            //
+
+        }
+        #region Kintamieji
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        string pub;
+        string _privateKeyXML;
+        string _publicKeyXml;
+        byte[] plaintext;
+        byte[] encryptedtext;
+        string Path;
+        Dictionary<string, string> keyDictionary = new Dictionary<string, string>();
+        #endregion Kintamieji
+
+        //
+        // DECRYPT MYGTUKAS
+        //
+        private void LibraryDecryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                // KOKIA TVARKA REIKIA UZKODUOTI BYTES, KAD TEISINGAI VISA INFORMACIJA PERSIDUOTU:
+                //
+                // abc
+                Encoding.UTF8.GetBytes
+                // 13 31 32
+                Convert.ToBase64String;
+                // saf
+                Convert.FromBase64String;
+                // 13 31 32
+                Encoding.UTF8.GetString;
+                // abc
+                */
+                byte[] inputBytes;
+                var encryptedBytes = Convert.FromBase64String(LibraryEncodedTextBox.Text);
+                using (var rsaClient = new RSACryptoServiceProvider(2048))
+                {
+                    //
+                    // PADUODU PRIVATE KEY IR ATSIFRUOJU
+                    //
+                    rsaClient.FromXmlString(_privateKeyXML);
+                    inputBytes = rsaClient.Decrypt(encryptedBytes, false);
+                }
+                string output = Encoding.UTF8.GetString(inputBytes);
+                LibraryRawTextBox.Text = output;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+
+        //
+        // SIFRAVIMO MYGTUKAS
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var rsaServer = new RSACryptoServiceProvider(2048))
+                {
+                    _privateKeyXML = rsaServer.ToXmlString(true);
+                    _publicKeyXml = rsaServer.ToXmlString(false);
+                }
+
+                //
+                // UZSIFRUOJA
+                //
+                string tempString = LibraryRawTextBox.Text;
+                var inputBytes = Encoding.UTF8.GetBytes(tempString);
+                byte[] encryptedBytes = null;
+                using (var rsaClient = new RSACryptoServiceProvider(2048))
+                {
+                    rsaClient.FromXmlString(_publicKeyXml);
+                    encryptedBytes = rsaClient.Encrypt(inputBytes, false);
+                    using (var sfd = new SaveFileDialog())
+                    {
+                        sfd.Filter = "txt files (*.txt)|*.txt";
+                        sfd.RestoreDirectory = true;
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+                            //
+                            // ISSAUGOJAMI BYTES I FILE, tarpinis zingsnis, tiesiog kodavimo eigoje taip patogiau buvo bytes perduoti
+                            //
+                            Path = sfd.FileName;
+                            File.WriteAllBytes(Path, encryptedBytes);
+                            LibraryEncodedTextBox.Text = Convert.ToBase64String(File.ReadAllBytes(Path));
+                            //
+                            // DICTIONARY tam, kad issaugomas file ir atsifravimui skirtas private key
+                            //
+                            keyDictionary.Add(_privateKeyXML, LibraryEncodedTextBox.Text);
+                            //
+                            // IRASOMA I FILE DICTIONARY TURINYS, jo mums reikes atsifruoti is failo, jei noresime
+                            //
+                            using (StreamWriter file = new StreamWriter(Path))
+                                foreach (var entry in keyDictionary)
+                                    file.WriteLine("{0} {1}", entry.Key, entry.Value);
+                        }
+                    }
+                }
+
+
+                // 
+                // UZSAUGOMAS PUBLIC KEY I FAILA
+                //
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\kajus\\Desktop\\saugumas\\test Praktinis 3\\publicKey.txt"))
+                {
+                    writer.Write(_publicKeyXml);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+
+        //
+        // DECRYPT IS FAILO, persikeliant i .net core su pavadinimais kylo problemu
+        //
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var content = string.Empty; ;
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "txt files (*.txt)|*.txt";
+                ofd.RestoreDirectory = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //
+                    // UZPILDOME DICTIONARY
+                    //
+                    Path = ofd.FileName;
+                    keyDictionary.Clear();
+                    string temp1;
+                    StreamReader sr = new StreamReader(Path);
+                    while ((temp1 = sr.ReadLine()) != null)
+                    {
+                        //
+                        // temp2 = private key
+                        // temp3 = uzkoduotas turinys
+                        //
+                        string temp2 = temp1.Substring(0, temp1.IndexOf(" "));
+                        string temp3 = temp1.Substring(temp1.IndexOf(" ") + 1);
+
+                        keyDictionary.Add(temp2, temp3);
+                    }
+                }
+            }
+            try
+            {
+                //
+                // PADUODU FAILO INFORMACIJA
+                // PRIVATUS RAKTAS (desifravimui)  --  UZSIFRUOTAS TURINYS
+                // PAVERCIU IS BASE64 KODUOTES I BYTES ATGAL TURINI
+                // VISKA PADUODU UZSIFRUOTI
+                //
+                string tempKey = keyDictionary.Keys.First();
+                byte[] inputBytes;
+                string tempValue = keyDictionary.Values.First();
+                byte[] tempValueBytes = Convert.FromBase64String(tempValue);
+                using (var rsaClient = new RSACryptoServiceProvider(2048))
+                {
+                    rsaClient.FromXmlString(tempKey);
+                    inputBytes = rsaClient.Decrypt(tempValueBytes, false);
+                }
+                LibraryRawTextBox.Text = Encoding.UTF8.GetString(inputBytes);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+    }
+}
